6d81db9ac0119551d5ff0d9bbc2223bcb91ddae0
Adam Akhtar <adam.akhtar@gmail.com>
2015-06-25 10:17:29 +0900

    add test: admin can view giveaways

---
+++ spec/features/admin_views_giveaways_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+feature "Admin views giveaways" do
+  before do
+    @admin = create(:user, :admin)
+    @ipad_giveaway = create(:giveaway, title: 'Ipad giveaway')
+    @xbox_giveaway = create(:giveaway, title: 'Xbox giveaway')
+  end
+
+  scenario "via dashboard" do
+    visit giveaways_path
+
+    expect(page).to have_content @ipad_giveaway.title
+    expect(page).to have_content @xbox_giveaway.title
+
+    click_link @ipad_giveaway.title
+
+    expect(current_path).to eq giveaway_path(@ipad_giveaway)
+    expect(page).to have_content(@ipad_giveaway.title)
+  end
+end
