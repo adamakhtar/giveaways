1a761375dd9f999978e299eafe2911288331dfae
Adam Akhtar <adam.akhtar@gmail.com>
2015-09-06 21:56:27 +0900

    create installer and refactor stylesheets to fit install process.
    # 50-character subject line
    #
    # 72-character wrapped longer description. This should answer:
    #
    # * Why was this change necessary?
    # * How does it address the problem?
    # * Are there any side effects?
    #
    # Include a link to the ticket, if any.

---
+++ lib/generators/giveaways/install/templates/initializer.rb
@@ -0,0 +1,6 @@
+Giveaways.main_app_path = '/'
+
+# To use your own layouts ucomment and set them below 
+# Giveaways.public_layout = 'layouts/giveaways'
+# Giveaways.main_app_path = '/some_path'
+
---
+++ lib/generators/giveaways/install_generator.rb
@@ -0,0 +1,144 @@
+require 'rails/generators'
+module Giveaways
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      class_option "user-class", :type => :string
+      class_option "no-migrate", :type => :boolean
+      class_option "current-user-helper", :type => :string
+
+      source_root File.expand_path("../install/templates", __FILE__)
+      desc "Used to install Giveaways"
+
+      def install_migrations
+        puts "Copying over Giveaways migrations..."
+        Dir.chdir(Rails.root) do
+          `rake giveaways:install:migrations`
+        end
+      end
+
+      def determine_current_user_helper
+        current_user_helper = options["current-user-helper"].presence ||
+                              ask("What is the current_user helper called in your app? [current_user]").presence ||
+                              :current_user
+
+        puts "Defining giveaways_user method inside ApplicationController..."
+
+        giveaways_user_method = %Q{
+  def giveaways_user
+    #{current_user_helper}
+  end
+  helper_method :giveaways_user
+
+}
+
+        inject_into_file("#{Rails.root}/app/controllers/application_controller.rb",
+                         giveaways_user_method,
+                         :after => "ActionController::Base\n")
+
+      end
+
+
+
+      def add_giveaways_initializer
+        path = "#{Rails.root}/config/initializers/giveaways.rb"
+        if File.exists?(path)
+          puts "Skipping config/initializers/giveaways.rb creation, as file already exists!"
+        else
+          puts "Adding giveaways initializer (config/initializers/giveaways.rb)..."
+          template "initializer.rb", path
+          require path 
+        end
+      end
+
+      def run_migrations
+        unless options["no-migrate"]
+          puts "Running rake db:migrate"
+          `rake db:migrate`
+        end
+      end
+
+      def mount_engine
+        puts "Mounting Giveaways::Engine at \"/giveaways\" in config/routes.rb..."
+        insert_into_file("#{Rails.root}/config/routes.rb", :after => /routes.draw.do\n/) do
+          %Q{
+  mount Giveaways::Engine, :at => '/giveaways'
+}
+        end
+      end
+
+      def create_assets
+        create_file Rails.root + "vendor/assets/stylesheets/giveaways.css.scss" do
+          %Q{ 
+@import "giveaways/base";
+}
+        end
+      end
+
+      def finished
+        output = "\n\n" + ("*" * 53)
+        output += %Q{\nDone! Giveaways has been successfully installed.
+
+Here's what happened:\n\n}
+
+        output += step("Giveaway's migrations were copied over into db/migrate.\n")
+        output += step("A new method called `giveaways_user` was inserted into your ApplicationController.
+   This lets Giveaways know what the current user of your application is.\n")
+        output += step("A new file was created at config/initializers/forem.rb
+   This is where you put Giveaway's configuration settings.\n")
+
+        unless options["no-migrate"]
+          output += step("`rake db:migrate` was run, running all the migrations against your database.\n")
+        end
+        output += step("The engine was mounted in your config/routes.rb file using this line:
+
+        mount Giveaways::Engine, :at => \"/giveaways\"
+
+If you want to change where the giveaways are located, just change the \"/giveaways\" path at the end of this line to whatever you want.\n\n˜")
+        
+        output +="Final Steps\n"
+        output +="===========\n\n"
+
+        unless defined?(Devise)
+          output += %Q{- We have detected you're not using Devise (which is totally OK), so you'll need to define a "sign_in_path" method. This method should return a path which points to the sign in path for your application. You can define it in config/routes.rb file with a line like this:
+
+          get '/users/sign_in', :to => "users#sign_in"
+\n\n}
+        end
+
+        output += "- Define a method to determine which users can access the Giveaways admin area.\n"
+        output += %Q{ 
+
+          class User
+            def can_manage_giveaways?
+              # e.g.
+              # return true if ( admin? or moderator? )
+            end
+          end
+
+That's it. You are ready to go!
+        }
+
+        output += "Thanks for using Giveaways!"
+        puts output
+      end
+
+      private
+
+      def step(words)
+        @step ||= 0
+        @step += 1
+        "#{@step}) #{words}\n"
+      end
+
+      def user_class
+        @user_class
+      end
+
+      # def next_migration_number
+      #   last_migration = Dir[Rails.root + "db/migrate/*.rb"].sort.last.split("/").last
+      #   current_migration_number = /^(\d+)_/.match(last_migration)[1]
+      #   current_migration_number.to_i + 1
+      # end
+    end
+  end
+end
