ac4ea0153685b230e3b29c367a6fd87c5f148db2
Adam Akhtar <adam.akhtar@gmail.com>
2015-09-06 22:45:45 +0900

    add example setting for admin_layout in initializer template
    # 50-character subject line
    #
    # 72-character wrapped longer description. This should answer:
    #
    # * Why was this change necessary?
    # * How does it address the problem?
    # * Are there any side effects?
    #
    # Include a link to the ticket, if any.

--- lib/generators/giveaways/install/templates/initializer.rb
+++ lib/generators/giveaways/install/templates/initializer.rb
@@ -2,5 +2,6 @@ Giveaways.main_app_path = '/'
 
 # To use your own layouts ucomment and set them below 
 # Giveaways.public_layout = 'layouts/giveaways'
+# Giveaways.admin_layout = 'layouts/admin'
 # Giveaways.main_app_path = '/some_path'
 
